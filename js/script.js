9f8d42ab0039800ed2e494803624137950f28a57
Amelia Brown <ameliabrown@Amelia-Browns-MacBook-Pro.local>
2016-01-14 16:56:21 -0500

    (feature): add javascript remove function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 var template = document.querySelector('#list-item').innerHTML;
 var newButton = document.getElementById("new-action");
+var createNew = document.getElementById("new");
 
 
-
-newButton.addEventListener('click', create, false)
+newButton.addEventListener('click', create, false);
+//createNew.addEventListener('keypress', create, false);
 
 function create() {
+    //if (event.keycode !== 13){
+    //  return;
+    //}
     //alert(text);
     var text = document.getElementById("new").value;
     var item = document.createElement("div");
@@ -15,4 +19,27 @@ function create() {
 
     list.appendChild(item);
     var text = document.getElementById("new").value = " ";
+
+    var close = document.querySelector(".ex");
+    close.addEventListener('click', remove, false);
+
+
+
+}
+function remove(e){
+  //var me = e.target.className;
+  //alert(me);
+  var actionI = e.target.parentNode.parentNode.parentNode;
+  //alert(actionI.tagName);
+  var actionUL = e.target.parentNode.parentNode.parentNode.parentNode;
+  actionUL.removeChild(actionI);
+  /*
+*event.target
+target.parent
+target.parent > parent > parent
+parent.class == list-item
+ ul.removeChild(list item element)
+*
+* */
+
 }
