e6fc9e3381b5ac78f551ee4186688acabe4ec3f0
Amelia Brown <ameliabrown@Amelia-Browns-MacBook-Pro.local>
2016-01-14 00:03:37 -0500

    (feature) added js form input function to create list items

--- js/script.js
+++ js/script.js
@@ -1,45 +1,18 @@
-'use strict';
-
-function putText(){
-  var text = document.getElementById("new").value;
-
-  if (text.length < 1){
-    return
-    //alert("please enter an item");
-  }else{
-    
-    //alert(text);
-  }
-}
-
-
+var template = document.querySelector('#list-item').innerHTML;
 var newButton = document.getElementById("new-action");
 
-newButton.addEventListener('click', putText, false)
-
-function newListItem() {
-  var listItem = document.createElement("LI");
-  listItem.className = "list-item";
-  var actionItems = document.createElement("DIV");
-  actionItems.className = "action-items";
-  var check = document.createElement("span");
-  check.className = "action check";
-  var ex = document.createElement("span");
-  ex.className = "action ex";
-  var checkIcon = document.createElement("I");
-  checkIcon.className = "fa fa-check";
-  var exIcon = document.createElement("I");
-  exIcon,setAttribute(class, "fa fa-times");
 
 
+newButton.addEventListener('click', create, false)
 
-  var list = document.getElementById("list");
+function create() {
+    //alert(text);
+    var text = document.getElementById("new").value;
+    var item = document.createElement("div");
+    item.innerHTML = template;
+    var listText = item.querySelector('.list-item').innerHTML = text;
+    var list = document.querySelector('#list');
 
-  list.appendChild(listItem);
-  list.appendChild(actionItems);
-  actionItems.appendChild(check);
-  check.appendChild(checkIcon);
-  actionItems.appendChild(ex);
-  ex.appendChild(exIcon);
-
+    list.appendChild(item);
+    var text = document.getElementById("new").value = " ";
 }
