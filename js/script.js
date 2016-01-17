558c5b9076df340484bf530f8f22c1cb62902931
Amelia Brown <ameliabrown@Amelia-Browns-MacBook-Pro.local>
2016-01-17 00:34:16 -0500

    (feature): add completion, add deletion, and enter to add

--- js/script.js
+++ js/script.js
@@ -1,40 +1,47 @@
 var template = document.querySelector('#list-item').innerHTML;
 var newButton = document.getElementById("new-action");
 var createNew = document.getElementById("new");
+var actionUL = document.getElementById("list");
 
 
 newButton.addEventListener('click', create, false);
-//createNew.addEventListener('keypress', create, false);
+createNew.addEventListener('keypress', create, false);
 
-function create() {
-    //if (event.keycode !== 13){
-    //  return;
-    //}
+function create(event) {
+    if (event.keyCode !== 13){
+      return;
+    }
     //alert(text);
     var text = document.getElementById("new").value;
     var item = document.createElement("li");
     item.innerHTML = template;
     var listText = item.querySelector('.list-item').innerHTML = text;
-    var list = document.querySelector('#list');
-    list.appendChild(item);
+    actionUL.insertBefore(item, actionUL.firstChild);
     var text = document.getElementById("new").value = " ";
     var close = item.querySelector(".ex");
     close.addEventListener('click', remove, false);
+    var checkmark = item.querySelector(".check");
+    checkmark.addEventListener('click', check, false);
 }
 function remove(e){
   //var me = e.target.className;
   //alert(me);
-  var actionI = e.target.parentNode.parentNode;
-  //alert(actionI.tagName);
-  var actionUL = e.target.parentNode.parentNode.parentNode;
-  actionUL.removeChild(actionI);
-  /*
-*event.target
-target.parent
-target.parent > parent > parent
-parent.class == list-item
- ul.removeChild(list item element)
-*
-* */
+  var target;
+  if(e.target.classList.contains("fa")){
+    target = e.target.parentNode.parentNode.parentNode;
+  }else{
+    target = e.target.parentNode.parentNode;
+  }//alert(actionI.tagName);
+  actionUL.removeChild(target);
+}
 
+function check(e){
+  var target;
+  if(e.target.classList.contains("fa")){
+    target = e.target.parentNode.parentNode.previousElementSibling;
+  }else{
+    target = e.target.parentNode.previousElementSibling;
+  }
+  debugger;
+  target.classList.add("strikethrough");
 }
